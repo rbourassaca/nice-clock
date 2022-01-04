f46c4e79cef875b8ee527feaa325d80162612bb2
Raphael Bourassa <bourassa.raphael@outlook.com>
2022-01-03 23:09:15 -0500

    Amélioration de la condition.

--- src/hooks/useTime.js
+++ src/hooks/useTime.js
@@ -12,9 +12,9 @@ export const useTime = () => {
 			setSeconds(d.getSeconds());
 			if (d.getSeconds() === 0) {
 				setMinutes(d.getMinutes());
-			}
-			if (d.getMinutes() === 0) {
-				setHours(d.getHours());
+				if (d.getMinutes() === 0) {
+					setHours(d.getHours());
+				}
 			}
 		}, 1000);
 		return () => clearInterval(tick);
