6186300515f95598d0826a8942ee2e4d76869c6c
Raphael Bourassa <bourassa.raphael@outlook.com>
2022-01-03 19:30:47 -0500

    Mise en place des "styled-components" à l'extérieur de la fonction Menu.

--- src/components/menu/menu.js
+++ src/components/menu/menu.js
@@ -3,40 +3,40 @@ import styled from "styled-components";
 import gear from "bootstrap-icons/icons/gear-fill.svg";
 import clock from "bootstrap-icons/icons/clock-fill.svg";
 
+const Nav = styled.nav`
+	position: absolute;
+	right: 0;
+	bottom: 0;
+	display: flex;
+	flex-direction: column;
+`;
+
+const Ul = styled.ul`
+	list-style-type: none;
+	margin: 1rem;
+`;
+
+const Li = styled.li`
+	&:not(:last-child) {
+		margin-bottom: 0.5rem;
+	}
+`;
+
+const A = styled(Link)`
+	&:link,
+	&:visited,
+	&:hover,
+	&:active {
+		text-decoration: none;
+		color: black;
+	}
+`;
+
+const Img = styled.img`
+	width: 3rem;
+`;
+
 const Menu = () => {
-	const Nav = styled.nav`
-		position: absolute;
-		right: 0;
-		bottom: 0;
-		display: flex;
-		flex-direction: column;
-	`;
-
-	const Ul = styled.ul`
-		list-style-type: none;
-		margin: 1rem;
-	`;
-
-	const Li = styled.li`
-		&:not(:last-child) {
-			margin-bottom: 0.5rem;
-		}
-	`;
-
-	const A = styled(Link)`
-		&:link,
-		&:visited,
-		&:hover,
-		&:active {
-			text-decoration: none;
-			color: black;
-		}
-	`;
-
-	const Img = styled.img`
-		width: 3rem;
-	`;
-
 	return (
 		<Nav>
 			<Ul>
